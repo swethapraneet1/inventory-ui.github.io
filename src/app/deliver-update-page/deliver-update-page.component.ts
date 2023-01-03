e9a8f10e34a8113d489f15d3bb5c6203a2502b4a
swetha <saisamba@cisco.com>
2023-01-03 06:23:38 +0530

    temp

---
+++ src/app/deliver-update-page/deliver-update-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deliver-update-page',
+  templateUrl: './deliver-update-page.component.html',
+  styleUrls: ['./deliver-update-page.component.scss']
+})
+export class DeliverUpdatePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
